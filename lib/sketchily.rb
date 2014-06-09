dbc8ab425a9518ebff3714ecd3b0527b147c87d7
Steven Bengtson <steven@Stevens-MacBook-Pro.local>
2014-06-09 14:01:43 -0700

    Upgraded svg-edit to 2.7

--- lib/sketchily.rb
+++ lib/sketchily.rb
@@ -3,7 +3,7 @@ require "sketchily/sketchily"
 require "sketchily/sketchily_tag"
 
 module Sketchily
-  ASSET_FILES = %w(sketchily.css sketchily.js sketchily_embed.js svg-edit-2.6/canvg/canvg.js svg-edit-2.6/canvg/rgbcolor.js svg-edit-2.6/extensions/ext-*.js svg-edit-2.6/locale/lang.*.js)
+  ASSET_FILES = %w(sketchily.css sketchily.js sketchily_embed.js svg-edit-2.7/canvg/canvg.js svg-edit-2.7/canvg/rgbcolor.js svg-edit-2.7/extensions/ext-*.js svg-edit-2.7/locale/lang.*.js)
 
   SKETCHILY_OPTIONS = [:width, :height, :bkgd_color, :bkgd_url, :canvas_width, :canvas_height, :canvas_expansion, :hide_rulers, :hide_menu, :hide_image_tool, :show_hyperlink_tool, :show_layers, :url, :extensions]
   TAG_OPTIONS = [:id, :value, :index]
@@ -13,4 +13,3 @@ module Sketchily
     Rails.configuration.assets.precompile += ASSET_FILES
   end
 end
-
